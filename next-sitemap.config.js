337da4583bab0a524485943eed3af164ec426781
Saba Morris <tradermajidi77@gmail.com>
2026-03-09 13:26:08 +0330

    fix(seo): use canonical www domain in sitemap config
    siteUrl was 'https://typegone.com' which generated sitemap URLs that
    307-redirect to www, causing Google Search Console redirect errors on
    all 8 indexed pages.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://typegone.com',
+  siteUrl: 'https://www.typegone.com',
   generateRobotsTxt: true,
   changefreq: 'weekly',
   priority: 0.7,
